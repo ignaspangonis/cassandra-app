1aac20a1dc52e7ca131d860438d4ea890813d251
ignaspangonis <ignas.pangonis@mif.stud.vu.lt>
2022-11-01 11:39:59 +0200

    Adjust messages

--- app.ts
+++ app.ts
@@ -26,22 +26,22 @@ async function main() {
     logBlue('> Ended building tables')
 
     await executeAndLog({
-      message: '1. Show all users',
+      message: '1. Get all users',
       query: 'SELECT * FROM users;'
     })
 
     await executeAndLog({
-      message: '2. Show all items with title "Nike shoes"',
+      message: '2. Get all items with title "Nike shoes"',
       query: "SELECT * FROM items_by_title WHERE title = 'Nike shoes';"
     })
 
     await executeAndLog({
-      message: '3. Show all items for owner with id 1',
+      message: '3. Get all items for owner with id 1',
       query: 'SELECT * FROM items_by_owner_id WHERE ownerId = 1;'
     })
 
     await executeAndLog({
-      message: '4. Show all users with name John born after 2022-01-01',
+      message: '4. Get all users with name John born after 2022-01-01',
       query:
         "SELECT * FROM users_by_name WHERE name='John' AND birthDate > toDate('2002-01-01');"
     })
